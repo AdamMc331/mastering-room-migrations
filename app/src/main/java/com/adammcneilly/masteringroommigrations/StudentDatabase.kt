f2d2f6fb848e582684203f6b7e44de65691185bf
adammc331 <amcneilly331@gmail.com>
2020-05-02 12:11:08 -0400

    Adding method to create a database.

--- app/src/main/java/com/adammcneilly/masteringroommigrations/StudentDatabase.kt
+++ app/src/main/java/com/adammcneilly/masteringroommigrations/StudentDatabase.kt
@@ -1,9 +1,23 @@
 package com.adammcneilly.masteringroommigrations
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Student::class], version = 1)
 abstract class StudentDatabase : RoomDatabase() {
     abstract fun studentDAO(): StudentDAO
+
+    companion object {
+        fun createDatabase(appContext: Context): StudentDatabase {
+            return Room.databaseBuilder(
+                appContext,
+                StudentDatabase::class.java,
+                "student-database.db"
+            )
+                .allowMainThreadQueries()
+                .build()
+        }
+    }
 }
