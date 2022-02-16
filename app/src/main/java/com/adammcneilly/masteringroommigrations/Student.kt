416b4376247b97a432852ffb0576f48170e2367a
keronei <lincolnkeronei95@gmail.com>
2022-02-16 14:50:44 +0300

    Student Entity Modification for v2
    Put back lastName, this will be omitted in v3, this ensured consistency in schema definition for
    refactoring.
    Added @ColumnInfo annotation to age in order to provide default value that room is aware of.

--- app/src/main/java/com/adammcneilly/masteringroommigrations/Student.kt
+++ app/src/main/java/com/adammcneilly/masteringroommigrations/Student.kt
@@ -1,5 +1,6 @@
 package com.adammcneilly.masteringroommigrations
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -8,5 +9,9 @@ data class Student(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
     val firstName: String = "",
+
+    val lastName : String = "",
+
+    @ColumnInfo(defaultValue = "0.0")
     val age: Double = 0.0
 )
